ec2bdcef4612b3a942f024f6b3b6288795505af1
Pedro Santos <pedrosantos@Pedros-MacBook-Pro.local>
2021-08-25 17:34:57 -0300

    Add theme in project

--- App.tsx
+++ App.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
 
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold
+} from '@expo-google-fonts/poppins';
+
 import theme from './src/global/styles/theme';
 import { Dashboard } from './src/screens/Dashboard';
 
