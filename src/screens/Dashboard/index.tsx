fa19764feb6d8e0db943d6dbe0d7cfb7b7c72d16
PedroSantosRocha <pedrosantos.ti.web@gmail.com>
2021-08-31 18:44:22 -0300

    using proportions in measures

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,11 +1,31 @@
 import React from 'react';
 
-import { Container, Title } from './styles';
+import {
+  Container,
+  Header,
+  UserWrapper,
+  UserInfo,
+  Photo,
+  User,
+  UserGreeting,
+  UserName,
+} from './styles';
 
 export function Dashboard() {
   return (
     <Container>
-      <Title>Dashboard</Title>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo source={{ uri: 'https://avatars.githubusercontent.com/u/50682093?v=4' }}/>
+
+            <User>
+              <UserGreeting>Ola,</UserGreeting>
+              <UserName>Pedro</UserName>
+            </User>
+          </UserInfo>
+        </UserWrapper>
+      </Header>
     </Container>
   );
 }
