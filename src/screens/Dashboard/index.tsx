ee1aeca2f533ba1d381147409a643639124b38cd
PedroSantosRocha <pedrosantos.ti.web@gmail.com>
2021-09-02 15:48:20 -0300

    leaving dynamic transaction cards

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -23,31 +23,34 @@ import {
 export function Dashboard() {
   const data = [
     {
-    title: "Desenvolvimento de site",
-    amount: "R$ 12.000,00",
-    category: {
-      name: "Vendas",
-      icon: "dollar-sign"
-    },
-    date: "13/04/2020",
+      type: "positive",
+      title: "Desenvolvimento de site",
+      amount: "R$ 12.000,00",
+      category: {
+        name: "Vendas",
+        icon: "dollar-sign"
+      },
+      date: "13/04/2021",
   },
     {
-    title: "Desenvolvimento de site",
-    amount: "R$ 12.000,00",
-    category: {
-      name: "Vendas",
-      icon: "dollar-sign"
-    },
-    date: "13/04/2020",
+      type: "negative",
+      title: "Hamburgeria Pizzy",
+      amount: "R$ 59,00",
+      category: {
+        name: "Alimentação",
+        icon: "coffee"
+      },
+      date: "10/04/2021",
   },
     {
-    title: "Desenvolvimento de site",
-    amount: "R$ 12.000,00",
-    category: {
-      name: "Vendas",
-      icon: "dollar-sign"
-    },
-    date: "13/04/2020",
+      type: "negative",
+      title: "Aluguel do apartamento",
+      amount: "R$ 1.200,00",
+      category: {
+        name: "Casa",
+        icon: "shopping-bag"
+      },
+      date: "06/04/2021",
   },
 ]
 
