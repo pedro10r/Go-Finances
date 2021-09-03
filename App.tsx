e48944a9d87c5a3974c16378a38ec298699e802b
PedroSantosRocha <pedrosantos.ti.web@gmail.com>
2021-09-03 17:54:10 -0300

    feat: sreen CategorySelect

--- App.tsx
+++ App.tsx
@@ -10,7 +10,8 @@ import {
 } from '@expo-google-fonts/poppins';
 
 import theme from './src/global/styles/theme';
-import { Register } from './src/screens/Register';
+
+import { CategorySelect } from './src/screens/CategorySelect';
 
 export default function App() {
   const [loadedFonts] = useFonts({
@@ -25,7 +26,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Register />
+      <CategorySelect />
     </ThemeProvider>
   );
 }
