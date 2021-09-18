f2784bdb98c66fb5ae2d703b698ef92a44364e7d
PedroSantosRocha <pedrosantos.ti.web@gmail.com>
2021-09-18 10:24:38 -0300

    use modal

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -66,7 +66,7 @@ export function Register() {
           </TransactionTypes>
 
           <CategorySelectButton
-            title="Categoria"
+            title={category.name}
             onPress={handleOpenSelectCategoryModal}
           />
         </Fields>
