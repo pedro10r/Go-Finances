f070765a27ba5806a4cb7940794333ec6ff29d28
Pedro Santos <pedrosantos.ti.web@gmail.com>
2022-10-19 18:40:08 -0300

    add unitary tests of test

--- src/__tests__/screens/profile.spec.tsx
+++ src/__tests__/screens/profile.spec.tsx
@@ -4,10 +4,30 @@ import { render } from '@testing-library/react-native';
 
 import { Profile } from "../../screens/Profile";
 
-test('Check if show correctly user input name placeholder', () => {
-  const { getByPlaceholderText } = render(<Profile />);
-
-  const inputName = getByPlaceholderText('Nome');
-
-  expect(inputName.props.placeholder).toBeTruthy();
+describe('Profile Screen', () => {
+  it('should have placeholder correctly in user name input', () => {
+    const { getByPlaceholderText } = render(<Profile />);
+  
+    const inputName = getByPlaceholderText('Nome');
+  
+    expect(inputName).toBeTruthy();
+  });
+  
+  it('should be load user data', () => {
+    const { getByTestId } = render(<Profile />);
+  
+    const inputName = getByTestId('input-name');
+    const inputSurname = getByTestId('input-surname');
+    
+    expect(inputName.props.value).toEqual('Pedro');
+    expect(inputSurname.props.value).toEqual('Rocha');
+  });
+  
+  it('should exist title correclty', () => {
+    const { getByTestId } = render(<Profile />);
+  
+    const textTitle = getByTestId('text-title');
+  
+    expect(textTitle.props.children).toContain('Perfil');
+  });
 });
--- src/screens/Profile/index.tsx
+++ src/screens/Profile/index.tsx
@@ -4,15 +4,21 @@ import { Button, Text, TextInput, View } from "react-native";
 export function Profile() {
   return (
     <View>
-      <Text>Perfil</Text>
+      <Text testID='text-title'>
+        Perfil
+      </Text>
 
       <TextInput
+        testID="input-name"
         placeholder="Nome"
         autoCorrect={false}
+        value="Pedro"
       />
 
       <TextInput
+        testID="input-surname"
         placeholder="Sobrenome"
+        value="Rocha"
       />
 
       <Button
